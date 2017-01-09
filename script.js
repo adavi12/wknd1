cb897fda931abbfb01ff6c0bd7ec66db020fa0ba
Ahkillah Davis <ahkillahdavis@gmail.com>
2017-01-09 09:30:29 -0600

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("JS");
 var empArray = [];
-
+var monthlySalaryCost = 0;
 $(document).ready(function(){
   var employees = $("#employees");
   console.log(employees);
@@ -17,34 +17,43 @@ $(document).ready(function(){
     empform.forEach(function(input){
 
       formInfo[input.name] = input.value;
-
-
     });
+
+
     var employeeFName = '<td>' + formInfo.empFirstName + '</td>';
     var employeeLName = '<td>' + formInfo.empLastName + '</td><';
     var employeeIDNum ='<td>' + formInfo.empIDNum + '</td>';
     var employeeJobTitle = '<td>' + formInfo.empJobTitle + '</td>';
     var employeeSalary = '<td>' + formInfo.empSalary + '</td>';
-    var monthlySalaryCost = '<td>'+ parseFloat((formInfo.empSalary)/12).toFixed(2) + '</td>';
-    var $employee = $('<tr></tr>');
 
+
+  var $employee = $('<tr>');
     $employee.append(employeeFName);
     $employee.append(employeeLName);
     $employee.append(employeeIDNum);
     $employee.append(employeeJobTitle);
     $employee.append(employeeSalary);
-    $employee.append(monthlySalaryCost);
     employees.append($employee);
-
-    $("#empFirstName").val(" ");
-    $("#empLastName").val(" ");
-    $("#empIDNum").val(" ");
-    $("#empJobTitle").val(" ");
-    $("#empSalary").val(" ");
+    $employee.append('</tr>');
+    $("#empd").append($employee);
+  //another way to clear form
+    // $("#empFirstName").val(" ");
+    // $("#empLastName").val(" ");
+    // $("#empIDNum").val(" ");
+    // $("#empJobTitle").val(" ");
+    // $("#empSalary").val(" ");
+    clearForm();
   $("#deleteButton" ).click(function() {
    console.log("delete clicked");
    //clears the entire form
   //  ($employee).remove();
   });
+  monthlySalaryCost = parseFloat((formInfo.empSalary)/12).toFixed(2);
+  $("#monthlySalCost").text(monthlySalaryCost);
   });
+
 });
+//to clear form
+function clearForm(){
+  $('form').find('input[type=text]').val('');
+}
